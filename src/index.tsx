6c701f6c10f42c97c5d7b1da760c155ffbc6a790
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2022-11-18 14:29:40 +0100

    project fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom/client';
 import { firebaseConfig } from './firebase';
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
@@ -14,17 +15,14 @@ const firebaseApp = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(firebaseApp);
 export const firebaseDb = getFirestore(firebaseApp);
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <StoreProvider>
         <App />
       </StoreProvider>
     </BrowserRouter> 
-  </React.StrictMode>
+  </React.StrictMode>, document.getElementById('root')
 );
 
 
